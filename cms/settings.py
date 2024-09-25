b2e7a1c501a0712de1edb783552eec95d0c46d43
Rajibsigdel12345 <76434595+Rajibsigdel12345@users.noreply.github.com>
2024-09-25 17:19:44 +0545

    add requirements file

--- cms/settings.py
+++ cms/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@li+#%qbr+#+jyc^w$e!83)sot%%b!o=vowmy@)b6iv@-jwa@b'
+SECRET_KEY = config('SECRET_KEY', cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'user.CustomUser'
 # Application definition
